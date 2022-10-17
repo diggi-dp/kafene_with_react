b2d47e9b636c76fc19cb9771d3b142f77201d62d
diggi-dp <digpalsinghmandloi10@gmail.com>
2022-10-17 15:49:17 +0530

    commit

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+import Login from '../../components/Login'
+
+
+
+const HomePage = () => {
+
+  return (
+    <div>
+      <Login/>
+    </div>
+  )
+}
+
+export default HomePage;
---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,18 @@
+
+
+const initialState = {
+    loggedIn: false,
+}
+
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'set_login':
+            return { ...state, loggedIn: action.payload };
+
+        default:
+            return state;
+    }
+}
+
+export default authReducer;
